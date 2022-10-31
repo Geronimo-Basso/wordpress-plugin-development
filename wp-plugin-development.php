1d89f317ccf50628e0be812ecf14bce3349abd82
Gerónimo Basso <geronimobasso@MacBook-Pro-de-Geronimo.local>
2022-10-31 12:28:05 +0100

    Added the wp-plugin.development.php file

---
+++ wp-plugin-development.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: wp-plugin-development
+ * Plugin URI: https://github.com/Geronimo-Basso
+ * Description: Big brain time
+ * Version: 1.0
+ * Author: Geronimo Basso
+ * Author URI: https://github.com/Geronimo-Basso
+ * Requires PHP: 5.4
+ * Requires at least: 4.7
+ * Tested up to: 6.0
+ * Text Domain:
+ * Domain Path: /languages/
+ *
+ * WC requires at least: 3.5
+ * WC tested up to: 6.8
+ * Woo: 260061:ecaa2080668997daf396b8f8a50d891a
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+ */
